8e2cd592cb105e480bd21f8098393b30685b0a71
Ramanuj <ramanuj0705@gmail.com>
2023-12-03 05:44:10 +0530

    basic html and css page of zerodha

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonEl = document.querySelector('.button--home');
+
+const signupHandler = () => {
+    alert("This is working pretty fine");
+}
+
+buttonEl.addEventListener('click', signupHandler);
